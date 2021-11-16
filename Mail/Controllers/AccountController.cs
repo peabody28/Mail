3831fd03f32d47cffbc0e4dc3987ea36b1c5bc0e
Maksim Gorbachewski <gorbachewski.m@gmail.com>
2021-11-16 14:40:26 +0300

    first commit

---
+++ Mail/Controllers/AccountController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Mail.Models;
+
+namespace Mail.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = ORM.Search(model.Email);
+                
+                if (user != null)
+                {
+                    await Authenticate(model.Email); // аутентификация
+
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+                
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = ORM.Search(model.Email);
+                
+                if (user == null)
+                {
+                    user = new User();
+                    user.Email = model.Email;
+                    user.Password = model.Password;
+                    user.Id = ORM.Create(user);
+
+                    await Authenticate(model.Email); // аутентификация
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            }
+            return View(model);
+        }
+
+        private async Task Authenticate(string userName)
+        {
+            // создаем один claim
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+            // создаем объект ClaimsIdentity
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            // установка аутентификационных куки
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+    }
+}
---
+++ Mail/ORM.cs
@@ -0,0 +1,64 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using Mail.Models;
+using System.Data.Common;
+
+namespace Mail
+{
+    public static class ORM
+    {
+        public static MySqlConnection conn;
+
+        static ORM()
+        {
+            string host = "127.0.0.1";
+            int port = 3306;
+            string database = "mail";
+            string username = "root";
+            string password = "12345678";
+
+            // Connection String.
+            String connString = "Server=" + host + ";Database=" + database
+                + ";port=" + port + ";User Id=" + username + ";password=" + password;
+
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            conn.Open();
+
+            ORM.conn = conn;
+        }
+
+        public static User Search(string email)
+        {
+            MySqlCommand cmd = conn.CreateCommand();
+
+            // Set Command Text
+            cmd.CommandText = $"SELECT * FROM users WHERE email = '{MySqlHelper.EscapeString(email)}'";
+
+            User tmp = new();
+            using (DbDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    tmp.Id = reader.GetInt32(0);
+                    tmp.Email = reader.GetString(1);
+                    tmp.Password = reader.GetString(2);
+                }
+                else
+                    return null;
+            }
+
+            return tmp;
+        }
+
+        public static Int32 Create(User user)
+        {
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = $"INSERT INTO users(email, password) VALUES('{user.Email}', '{user.Password}')";
+            int id = cmd.ExecuteNonQuery();
+
+            return id;
+        }
+    }
+}
